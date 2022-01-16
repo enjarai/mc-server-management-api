27f38f165749425e1282f06441c5a5c63b0bb32c
enjarai <enjarai123@gmail.com>
2022-01-16 22:15:01 +0100

    add example

---
+++ examples/auth.py
@@ -0,0 +1,9 @@
+# all cookies received will be stored in the session object
+s = requests.Session()
+
+# authenticate with the api to get the jwt cookie. this will be valid for a maximum of one hour
+payload={'username'=<username>,'password'=<password>}
+s.post('http://example.com/api/users/login',data=payload)
+
+# using the same session, make subsequent queries
+players = s.get('http://example.com/api/players/smp').json()
